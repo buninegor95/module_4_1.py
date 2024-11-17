a65f6c47198a315e5b6230a19954663c043b152e
buninegor95 <buninegor95@gmail.com>
2024-11-17 20:43:04 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,10 @@
+def fake_divide(first, second):
+
+    ohibka = "ошибка"
+    if second == 0:
+        return ohibka
+    else:
+        answer = first / second
+        return answer
+
+
---
+++ true_math.py
@@ -0,0 +1,10 @@
+from math import inf
+
+def true_divide(first, second):
+
+    if second == 0:
+        return inf
+    else:
+        answer = first / second
+        return answer
+
